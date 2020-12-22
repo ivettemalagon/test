14ae4cd298378d1a50334d7a4e44cbe4d5a73561
ivettemalagon <71296531+ivettemalagon@users.noreply.github.com>
2020-12-22 11:49:47 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -128,72 +128,83 @@ export default {
 <style>
   body{
     margin: 0 0 0 0;
-    background-color:#414141;
   }
+
   .header{
     margin: 0%;
     padding: 0;
     width: 100%;
-    height: 10vh;
+    height: 10vh; 
     min-height: 100px;
-    background-color: #93BF47 ;
-    color:#414141 ;
+
+    background-color: #283747 ;
+    color:#E5E7E9  ;
+
     display: flex;
     justify-content: space-between;
     align-items: center;
   }
-  
+
+  .header h1{
+    width: 20%;
+    text-align: center;
+  }
+
   .header nav {
     height: 100%;
-    width: 45%;
+    width: 40%;
+
     display: flex;
     justify-content: space-around;
     align-items: center;
-    
+
+    font-size: 20px;
   }
+
   .header nav button{
-    color: #000000;
-    background: #93BF47;
-    border: 1px solid #93BF47;
+    color: #E5E7E9;
+    background: #283747;
+    border: 1px solid #E5E7E9;
+
     border-radius: 5px;
     padding: 10px 20px;
-    font-family: "Roboto-Bold", Helvetica, Arial, serif;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 700;
   }
+
   .header nav button:hover{
-    color: #414141;
-    background: #E6B06B;
-    border: 1px solid #E6B06B;
+    color: #283747;
+    background: #E5E7E9;
+    border: 1px solid #E5E7E9;
   }
+
+  
   .main-component{
     height: 75vh;
     margin: 0%;
     padding: 0%;
-    background: #414141 ;
+
+    background: #FDFEFE ;
   }
+
+ 
   .footer{
-    position:absolute;
     margin: 0;
     padding: 0;
     width: 100%;
-    height: 15vh;
-    min-height: 100px;
-    background-color: #93BF47;
-    color: #000000;
+    height: 10vh;
+    min-height: 100px; 
+
+    background-color: #283747;
+    color: #E5E7E9;
+
   }
+
   .footer h2{
     width: 100%;
     height: 100%;
-    left:calc(50.6% - 30px);
-    position:absolute;
+    
+    display: flex;
     justify-content: center;
     align-items: center;
-    font-family: "Roboto-Regular", Helvetica, Arial, serif;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 700;
   }
 
   }
