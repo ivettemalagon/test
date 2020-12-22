d9b8eae8a5dddd5fea8d9198c7f8bd0a8aa4bba2
ivettemalagon <71296531+ivettemalagon@users.noreply.github.com>
2020-12-22 12:35:53 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -214,7 +214,7 @@ export default {
   .footer h2{
     width: 100%;
     height: 100%;
-    
+    left:calc(39.16% - 30px);
     display: flex;
     position:absolute;
     justify-content: center;
