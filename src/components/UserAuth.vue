8505b7fba790d2a77f8fb6e4c544e1832e5cccf2
ivettemalagon <71296531+ivettemalagon@users.noreply.github.com>
2020-12-22 10:02:22 -0500

    Update UserAuth.vue

--- src/components/UserAuth.vue
+++ src/components/UserAuth.vue
@@ -41,7 +41,7 @@ export default {
     methods: {
         processAuthUser: function(){
             var self = this
-            axios.post("http://127.0.0.1:8000/user/auth/", self.user_in,  {headers: {}})
+            axios.post("https://infinapp-ui.herokuapp.com/", self.user_in,  {headers: {}})
                 .then((result) => {
                     alert("Autenticación Exitosa");
                     self.$emit('log-in', self.user_in.username)
